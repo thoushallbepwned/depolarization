1f9659f5dbcb7f2b2db15855bc55a57db510e80d
thoushallbepwned <verhagen.paul.j@gmail.com>
2023-08-22 10:12:20 +0200

    visualization_code

--- visualization_of_polarization_metrics.py
+++ visualization_of_polarization_metrics.py
@@ -1,8 +1,11 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
 
 def plot_net_difference(allowance):
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
     with open('simulation_results.pkl', 'rb') as f:
         loaded_results = pickle.load(f)
 
@@ -92,12 +95,15 @@ def plot_net_difference(allowance):
     plt.subplots_adjust(top=0.8)
     plt.tight_layout()
     plt.suptitle(f"Allowance: {allowance}", fontsize=16, y=1)
-    plt.show()
-    plt.show()
+    #plt.show()
+    #plt.show()
+    return fig
 
 
 
 def plot_allowance(allowance):
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
     with open('simulation_results.pkl', 'rb') as f:
         loaded_results = pickle.load(f)
     #print(loaded_results.keys())
@@ -113,7 +119,7 @@ def plot_allowance(allowance):
 
     # Extract all epsilon parameters
     param_values = set([key[4] for key in loaded_results.keys() if key[2] == allowance])
-    print(param_values)
+    #print(param_values)
 
 
     for interaction in interactions:
@@ -153,7 +159,7 @@ def plot_allowance(allowance):
 
             value = loaded_results[key_to_retrieve]
             alt_dict_value = value[1]
-            print(f"This is the value for parameter {param_value}", alt_dict_value)
+            #print(f"This is the value for parameter {param_value}", alt_dict_value)
 
             # Plot data for each scenario
             for i, scenario in enumerate(scenarios):
@@ -181,10 +187,13 @@ def plot_allowance(allowance):
     plt.subplots_adjust(top=0.8)
     plt.tight_layout()
     plt.suptitle(f"Allowance: {allowance}", fontsize=16, y=1)
-    plt.show()
+    #plt.show()
+    return fig
 
 
 def plot_interaction(interaction):
+    mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#9467BD', '#8C564B', '#E377C2', '#7F7F7F'])
+
     with open('simulation_results.pkl', 'rb') as f:
         loaded_results = pickle.load(f)
 
@@ -198,25 +207,26 @@ def plot_interaction(interaction):
     # Determine the global y-axis limits for % contribution
     all_y_percent_values = []
 
-    # Extract all epsilon parameters
-    param_values = set([key[4] for key in loaded_results.keys() if key[1] == interaction])
-    print(param_values)
-
     for allowance in allowances:
-        for param_value in [0.2]:
-            key_to_retrieve = ('noiseless', interaction, allowance, 'mixed', param_value)
 
-            if key_to_retrieve not in loaded_results:
-                continue
+        key_to_retrieve = ('noiseless', interaction, allowance, 'mixed', 0.20)
+        #print(key_to_retrieve)
+        if key_to_retrieve not in loaded_results:
+            print("We are getting stuck here")
+            continue
 
-            value = loaded_results[key_to_retrieve]
-            alt_dict_value = value[1]
+        value = loaded_results[key_to_retrieve]
+        alt_dict_value = value[1]
+        #print("This is the item", allowance, alt_dict_value)
 
-            all_y_values.extend([np.sum(entry[1]) for scenario in scenarios for entry in alt_dict_value[scenario]])
-            all_y_percent_values.extend(
-                [max(entry[1]) / np.sum(entry[1]) * 100 for scenario in scenarios for entry in alt_dict_value[scenario]])
+        all_y_values.extend([np.sum(entry[1]) for scenario in scenarios for entry in alt_dict_value[scenario]])
+        all_y_percent_values.extend(
+            [max(entry[1]) / np.sum(entry[1]) * 100 for scenario in scenarios for entry in alt_dict_value[scenario]])
+        #(all_y_values)
 
     y_min = min(all_y_values) - 0.1
+
+
     y_max = max(all_y_values) + 0.1
 
     y_percent_min = min(all_y_percent_values) - 2
@@ -262,8 +272,105 @@ def plot_interaction(interaction):
     plt.subplots_adjust(top=0.8)
     plt.tight_layout()
     plt.suptitle(f"Interaction: {interaction}", fontsize=16, y=1)
-    plt.show()
+    #plt.show()
+    return fig
 
+def plot_net_difference2(interaction):
+    mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#9467BD', '#8C564B', '#E377C2', '#7F7F7F'])
+
+    with open('simulation_results.pkl', 'rb') as f:
+        loaded_results = pickle.load(f)
+
+    # Provided options
+    allowances = ["strict_euclidean", "cosine", "mean_euclidean", "size_cosine"]
+    scenarios = ['low-removal', 'high-removal']
+
+
+    # Create subplots
+    fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+
+    # Determine the global y-axis limits for sum and % contribution
+    all_y_sum_diff_values = []
+    all_y_percent_diff_values = []
+
+    for allowance in allowances:
+
+        key_to_retrieve = ('noiseless', interaction, allowance, 'mixed', 0.20)
+
+        if key_to_retrieve not in loaded_results:
+            continue
+
+        value = loaded_results[key_to_retrieve]
+        alt_dict_value = value[1]
+
+        natural_values = alt_dict_value['natural']
+
+        for scenario in scenarios:
+            scenario_values = alt_dict_value[scenario]
+
+            # For sum differences
+            sum_diffs = [np.sum(entry[1]) - np.sum(natural_entry[1]) for entry, natural_entry in
+                         zip(scenario_values, natural_values)]
+            all_y_sum_diff_values.extend(sum_diffs)
+
+            # For % contribution differences
+            percent_diffs = [
+                max(entry[1]) / np.sum(entry[1]) * 100 - max(natural_entry[1]) / np.sum(natural_entry[1]) * 100 for
+                entry, natural_entry in zip(scenario_values, natural_values)]
+            all_y_percent_diff_values.extend(percent_diffs)
+
+    y_sum_diff_min = min(all_y_sum_diff_values) - 0.1
+    y_sum_diff_max = max(all_y_sum_diff_values) + 0.1
+
+    y_percent_diff_min = min(all_y_percent_diff_values) - 5
+    y_percent_diff_max = max(all_y_percent_diff_values) + 5
+
+    # Loop over each interaction option
+    for allowance in allowances:
+        #for allowance in ["strict_euclidean", "cosine"]:
+        key_to_retrieve = ('noiseless', interaction, allowance, 'mixed', 0.20)
+
+        if key_to_retrieve not in loaded_results:
+            continue
+
+        value = loaded_results[key_to_retrieve]
+        alt_dict_value = value[1]
+
+        natural_values = alt_dict_value['natural']
+
+        # Plot data for each scenario
+        for i, scenario in enumerate(scenarios):
+            scenario_values = alt_dict_value[scenario]
+
+            # For sum differences
+            x = [entry[0] for entry in scenario_values]
+            y_diff = [np.sum(entry[1]) - np.sum(natural_entry[1]) for entry, natural_entry in
+                      zip(scenario_values, natural_values)]
+            axs[0, i].plot(x, y_diff, '-o', label=f"{interaction} - {allowance}")
+            axs[0, i].set_title(f"Sum Difference - {scenario}")
+            axs[0, i].set_xlabel('Parameter Value')
+            axs[0, i].set_ylabel('Net Sum Difference')
+            axs[0, i].set_ylim(y_sum_diff_min, y_sum_diff_max)
+            axs[0, i].legend()
+
+            # For % contribution differences
+            y_percent_diff = [
+                max(entry[1]) / np.sum(entry[1]) * 100 - max(natural_entry[1]) / np.sum(natural_entry[1]) * 100 for
+                entry, natural_entry in zip(scenario_values, natural_values)]
+            axs[1, i].plot(x, y_percent_diff, '-o', label=f"{interaction} - {allowance}")
+            axs[1, i].set_title(f"% Contribution Difference - {scenario}")
+            axs[1, i].set_xlabel('Parameter Value')
+            axs[1, i].set_ylabel('Net % Contribution Difference')
+            axs[1, i].set_ylim(y_percent_diff_min, y_percent_diff_max)
+            axs[1, i].legend()
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.8)
+    plt.tight_layout()
+    plt.suptitle(f"Interaction: {interaction}", fontsize=16, y=1)
+    #plt.show()
+    #plt.show()
+    return fig
 
 # Example usage:
 
@@ -271,8 +378,18 @@ def plot_interaction(interaction):
 options = ["strict_euclidean", "cosine", "mean_euclidean", "size_cosine"]
 
 
-# for option in options:
-#     plot_allowance(option)
-#     plot_net_difference(option)
+for option in options:
+    print(option)
+    fig1= plot_allowance(option)
+    fig2 = plot_net_difference(option)
+    fig1.savefig(f"images/{option}_allowance.png")
+    fig2.savefig(f"images/{option}_allowance_diff.png")
 
-plot_interaction("ensemble")
+options2 = ["bounded", "ensemble", "softmax", "sequential"]
+for option in options2:
+    fig3 = plot_interaction(option)
+    fig4 = plot_net_difference2(option)
+    fig3.savefig(f"images/{option}_interaction.png")
+    fig4.savefig(f"images/{option}_interaction_difference.png")
+
+#plot_interaction("ensemble")
